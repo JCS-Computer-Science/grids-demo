deedae56b733cb2241ac7f82081db4fcf8984fbf
JoeTodes <jtwilson.home@gmail.com>
2024-03-04 14:53:14 -0700

    grids

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let canvasWidth = 400;
-let canvasHeight = 400;
-let rows = 3;
-let cols = 3;
+let canvasWidth = 300;
+let canvasHeight = 300;
+let rows = 10;
+let cols = 5;
 let grid = [];
 for (let i = 0; i < rows; i++) {
 	let row = [];
@@ -11,19 +11,19 @@ for (let i = 0; i < rows; i++) {
 	grid.push(row);
 }
 
-let cellWidth = canvasWidth / cols;
-let cellHeight = canvasHeight / rows;
+let cellWidth = canvasWidth / grid[0].length;
+let cellHeight = canvasHeight / grid.length;
+
 function setup() {
 	createCanvas(canvasWidth, canvasHeight);
-}
-function draw() {
-	background(100);
 	for (let i = 0; i < grid.length; i++) {
 		for (let j = 0; j < grid[i].length; j++) {
+			fill("white");
 			rect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
 			fill("black");
 			textSize(50);
-			text(grid[i][j], j * cellWidth + 35, i * cellHeight + 70);
+			text(grid[i][j], j * cellWidth + 30, i * cellHeight + 70);
 		}
 	}
 }
+function draw() {}
